da026f1727431141ed646f8e6c001de1d6338246
Amanuel <amanueleman@gmail.com>
2023-12-03 17:52:45 -0500

    added GET route to read db file

--- server.js
+++ server.js
@@ -20,12 +20,32 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// Get route which reads the db.json file and sends back the parsed JSON data
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+      console.log(jsonData);
+    });
+});
+
+
+
+
+
+  // Listen for connections
+app.listen(PORT, () =>
+console.info(`App listening at http://localhost:${PORT}`)
+);
+
